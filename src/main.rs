04327984041f4fa5d8d5dd868b38f42f5d09310d
Sudhanshu <perfectsudh@gmail.com>
2020-07-15 08:57:19 +0530

    Refactor: uses Result for better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("In file {}", config.filename);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents)
+    println!("With text:\n{}", contents);
+    Ok(())
 }
 
 struct Config {
@@ -20,12 +30,12 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("not enough arguments")
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
